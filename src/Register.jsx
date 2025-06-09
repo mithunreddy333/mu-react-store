34144c9717016d8de029c849be6b4a860804890b
mithunreddy03 <mithunreddy3836@gmail.com>
2025-06-09 15:39:28 +0530

    first commit

---
+++ src/Register.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+export default function Register() {
+  const [user, setUser] = useState({});
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    alert("Hello World");
+  };
+  const updateCount = () => {
+    setCount(count + 1);
+  };
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+  const decreaseCount=()=> {
+    setCount(count-1);
+  }
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <p>
+        <input
+          type="text"
+          placeholder="Enter Name"
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+        />
+      </p>
+      <p>
+        <input
+          type="text"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="Enter Email Address"
+        />
+      </p>
+      <p>
+        <input
+          type="password"
+          onChange={(e) => setUser({ ...user, pass: e.target.value })}
+          placeholder="New Password"
+        />
+      </p>
+      <p>
+        <button>Submit</button>
+      </p>
+      <hr />
+      <p>
+        <Link to="/login">Aready a member? Login Here...</Link>
+      </p>
+      <hr />
+      <button onClick={handleClick}>Click</button>
+      <hr />
+      <p>
+        {count}<br></br>
+        <button onClick={decreaseCount}>-</button>
+        <button onClick={updateCount}>Update Count</button>
+        <button onClick={increaseCount}>+</button>
+      </p>
+     
+    </div>
+  );
+}
