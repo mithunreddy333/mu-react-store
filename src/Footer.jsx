b57310432183cb61342e101bbe671f106ff3e284
mithunreddy03 <mithunreddy3836@gmail.com>
2025-06-10 16:04:56 +0530

    update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css"
 export default function Footer() {
   return (
     <div className="App-Footer-Row">
-      $copy; 2025. All rights reserved.
+      &copy; 2025. All rights reserved.
     </div>
   )
 }
